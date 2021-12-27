d2bba384bbaf5c84db2817badc99e65d63f7b73d
Beta5051 <beta5051@gmail.com>
2021-12-28 03:41:57 +0900

    토큰 생성 추가

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,3 +1,7 @@
+import 'package:crypto/crypto.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:uuid/uuid.dart';
+
 String EnumToString(Enum enumObject) => enumObject.toString().split('.')[1];
 
 T? StringToEnum<T extends Enum>(List<T> values, String s) {
@@ -17,4 +21,16 @@ String ListToString(List<dynamic> list) {
     }
   }
   return result;
+}
+
+String GenerateToken(String access_key, String secret_key, String query) {
+  final payload = {
+    'access_key': access_key,
+    'nonce': Uuid().v4(),
+  };
+  if (query.isNotEmpty) {
+    payload['query_hash'] = sha512.convert(query.codeUnits).toString();
+    payload['query_hash_alg'] = 'SHA512';
+  }
+  return JWT(payload).sign(SecretKey(secret_key));
 }
